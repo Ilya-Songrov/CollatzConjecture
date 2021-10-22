8d511f3b7bccde9f659d564cbca71012df595e7b
devbugless <ilyasongrov@gmail.com>
2021-10-22 11:54:25 +0300

    Fixed bugs

--- main1.cpp
+++ main1.cpp
@@ -2,30 +2,51 @@
 #include <ctime>
 #include <string>
 #include <regex>
+#include <cassert>
+
 
 #include <gmpxx.h>
 
+void runAsserts();
 int checkValidity(const int argc, char *argv[], mpz_class* input);
-void sequence(const mpz_class size, mpz_class* max_number);
-mpz_class sequence(const mpz_class& var);
+mpz_class maxSequenceFromRange(const mpz_class size);
+mpz_class getSequenceLength(const mpz_class& var);
 void printElapsedTime(const clock_t begin, const clock_t end);
 void printResult(const mpz_class max_number);
 
 int main(int argc, char *argv[])
 {
+    runAsserts();
+
     mpz_class input = 0;
     if (checkValidity(argc, argv, &input)) {
         return EXIT_FAILURE;
     }
-    mpz_class max_number = 0;
 
     const clock_t begin = clock();
-    sequence(input, &max_number);
+    const mpz_class max_number = maxSequenceFromRange(input);
     const clock_t end = clock();
 
     printResult(max_number);
     printElapsedTime(begin, end);
     return EXIT_SUCCESS;
+}
+
+void runAsserts()
+{
+    assert(getSequenceLength(-1) == 0);
+    assert(getSequenceLength(0) == 0);
+    assert(getSequenceLength(1) == 1);
+    assert(getSequenceLength(2) == 2);
+    assert(getSequenceLength(3) == 8);
+    assert(getSequenceLength(4) == 3);
+    assert(maxSequenceFromRange(-1) == 0);
+    assert(maxSequenceFromRange(0) == 0);
+    assert(maxSequenceFromRange(1) == 0);
+    assert(maxSequenceFromRange(2) == 1);
+    assert(maxSequenceFromRange(3) == 2);
+    assert(maxSequenceFromRange(4) == 3);
+    assert(maxSequenceFromRange(8) == 7);
 }
 
 int checkValidity(const int argc, char *argv[], mpz_class *input)
@@ -47,14 +68,17 @@ int checkValidity(const int argc, char *argv[], mpz_class *input)
     return EXIT_FAILURE;
 }
 
-void sequence(const mpz_class size, mpz_class *max_number)
+mpz_class maxSequenceFromRange(const mpz_class size)
 {
-    mpz_class top_number     = size - 1;
-    mpz_class _max_number    = top_number;
-    mpz_class _max_path      = mpz_tstbit(top_number.get_mpz_t(), 0) ? 0 : sequence(top_number);
-    top_number              = _max_path == 0 ? top_number : top_number - 1;
+    if (size < 2) {
+        return mpz_class(0);
+    }
+    mpz_class _top_number       = size - 1;
+    mpz_class _max_number       = _top_number;
+    mpz_class _max_path         = mpz_tstbit(_top_number.get_mpz_t(), 0) ? 0 : getSequenceLength(_top_number);
+    _top_number                 = _max_path == 0 ? _top_number : _top_number - 1;
 
-    for (mpz_class var = top_number;  var > 2;  var -= 2) {
+    for (mpz_class var = _top_number; var > 2;  var -= 2) {
         mpz_class number = var;
         mpz_class count = 2;
         while (true){
@@ -75,6 +99,9 @@ void sequence(const mpz_class size, mpz_class *max_number)
         }
 
         if (number == 2){
+            count += 2;
+        }
+        else if (number == 1){
             ++count;
         }
         if (count > _max_path) {
@@ -82,11 +109,18 @@ void sequence(const mpz_class size, mpz_class *max_number)
             _max_number = var;
         }
     }
-    *max_number     = _max_number;
+    return _max_number;
 }
 
-mpz_class sequence(const mpz_class &var)
+mpz_class getSequenceLength(const mpz_class &var)
 {
+    if (var < 1) {
+        return 0;
+    }
+    if (var == 1 || var == 2) {
+        return var;
+    }
+
     mpz_class number = var;
     mpz_class count = 2;
     while (true){
@@ -106,6 +140,9 @@ mpz_class sequence(const mpz_class &var)
         count += 2;
     }
     if (number == 2){
+        count += 2;
+    }
+    else if (number == 1){
         ++count;
     }
     return count;
